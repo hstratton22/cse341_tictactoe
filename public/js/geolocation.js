ecd3fcd995ba16f30010897edbb30371dc980d1b
Heather Stratton <hastratton@gmail.com>
2021-07-10 16:16:43 -0400

    more console.log

--- public/js/geolocation.js
+++ public/js/geolocation.js
@@ -1,7 +1,7 @@
 if ('geolocation' in navigator) {
-    console.log('geolocation available');
+   //console.log('geolocation available');
     navigator.geolocation.getCurrentPosition(function(position){
-    console.log('latitude:', position.coords.latitude, 'Longitude:', position.coords.longitude)
+    //console.log('latitude:', position.coords.latitude, 'Longitude:', position.coords.longitude)
     document.getElementById("latitude").value = position.coords.latitude;
     document.getElementById("longitude").value = position.coords.longitude;
     });
